dd6943f7ba265323dada486eb9c26a0871ee135e
Dan Sazonov <p.294803@gmail.com>
2021-10-19 18:48:57 +0300

    Написал высотомер

--- main/main.ino
+++ main/main.ino
@@ -3,25 +3,32 @@
 #include <Adafruit_BMP085_U.h>
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+int startAltitude;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("begin");
+  bmp.begin();
+
+  startAltitude = getAltitude();
 
 }
 
 void loop() {
+  Serial.println(getAltitude() - startAltitude);
+}
+
+
+float getAltitude() {
   sensors_event_t event;
   bmp.getEvent(&event);
-  if (event.pressure) {
+    if (event.pressure) {
     float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-    Serial.print("Altitude:    ");
-    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
-                                        event.pressure));
-    Serial.println(" m");
-    Serial.println("");
+    return bmp.pressureToAltitude(seaLevelPressure,event.pressure);
   }
   else
   {
     Serial.println("Sensor error");
+    return 0.0;
   }
 }
