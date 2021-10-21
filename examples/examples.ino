e05a7c51563877b76fb1dca3a032d28e8e5f6b1a
Dan Sazonov <p.294803@gmail.com>
2021-10-21 12:15:04 +0300

    Настроил интервалы отправки телеметрии

--- examples/examples.ino
+++ examples/examples.ino
@@ -1,17 +1,9 @@
 // переменная хранения времени (unsigned long)
 uint32_t myTimer1;
-int period = 1000;
-bool status_s = true;
-bool was_activated = false;
-
-void setup() {
-  Serial.begin(9600);
-  }
-
+void setup() {Serial.begin(9600);}
 void loop() {
-  if (millis() - myTimer1 >= 500 && !was_activated) {   // ищем разницу (500 мс)
-    was_activated = true;
-    status_s = false;
+  if (millis() - myTimer1 >= 500) {   // ищем разницу (500 мс)
+    myTimer1 = millis();              // сброс таймера
+    Serial.println("test");
   }
-  Serial.println(status_s);
 }
--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,6 @@
 #define srv_pin A3  // пин сервы
 #define photo_pin A6 // пин фоторезистора
+#define radio_vcc_pin A0 // пин питания радиомодуля
 #define srv_high_time 5000  // время работы сервы
 
 #include <Wire.h>
@@ -13,17 +14,21 @@ bool start_flag, sep_flag, rec_flag, land_flag = false;
 float current_altitude = 0.0;
 float max_altitude = 0.0;
 
-unsigned long last_time = 0;
+uint32_t srv_last_time = 0;
 bool srv_state = false;
 bool srv_timeout = false;
 
-int photo_val;
+int photo_val = 0;
+
+uint32_t radio_last_time = 0;
 
 void setup() {
   pinMode(srv_pin, OUTPUT);
   pinMode(photo_pin, INPUT);
+  pinMode(radio_vcc_pin, OUTPUT);
+
+  digitalWrite(radio_vcc_pin, HIGH);
   Serial.begin(9600);
-  Serial.println("begin");
   bmp.begin();
 
   photo_val = analogRead(photo_pin);
@@ -35,37 +40,40 @@ void loop() {
   // сохраняем значение прошлой высоты
   float last_altitude = current_altitude;
   current_altitude = getAltitude() - start_altitude;
-  
+
   // определяем необходимые точки, высчитываем максимальную высоту
   if (current_altitude - start_altitude >= 5.0) start_flag = true;
   if (current_altitude > max_altitude) max_altitude = current_altitude;
   if ((max_altitude - current_altitude >= 50)  && start_flag) rec_flag = true;
   if (last_altitude - current_altitude <= 2 && start_flag) land_flag = true;
   if (photo_val - analogRead(photo_pin) >= 10) sep_flag = true;
-  
+
   // запускаем серву на 2 секунды
   if (rec_flag && !srv_state) {
     srv_state = true && !srv_timeout;
-    last_time = millis();
+    srv_last_time = millis();
     digitalWrite(srv_pin, srv_state);
   }
-  if (millis() - last_time >= srv_high_time && srv_state) {
+  if (millis() - srv_last_time >= srv_high_time && srv_state) {
     srv_state = false;
     srv_timeout = true;
-}
+  }
 
   // отправляем телеметрию
-  Serial.println("CanSAD;" + String(millis()) + ";" + String(current_altitude) + ";" + String(start_flag) + ";" + String(sep_flag) + 
-                 ";" + String(rec_flag) + ";" + String(land_flag) + "\n");
+  if (millis() - radio_last_time >= 500) {
+    radio_last_time = millis();
+    Serial.println("CanSAD;" + String(millis()) + ";" + String(current_altitude) + ";" + String(start_flag) + ";" + String(sep_flag) +
+                   ";" + String(rec_flag) + ";" + String(land_flag) + "\n");
+  }
 }
 
 
 float getAltitude() {
   sensors_event_t event;
   bmp.getEvent(&event);
-    if (event.pressure) {
+  if (event.pressure) {
     float sea_level_pressure = SENSORS_PRESSURE_SEALEVELHPA;
-    return bmp.pressureToAltitude(sea_level_pressure,event.pressure);
+    return bmp.pressureToAltitude(sea_level_pressure, event.pressure);
   }
   else return 0.0;
 }
