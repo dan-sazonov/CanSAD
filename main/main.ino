0b0d1ca5976f0e50ab3a36f4c3ae9d52d42af0cd
Dan Sazonov <p.294803@gmail.com>
2021-10-21 11:58:44 +0300

    Запилил флаг приземления

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,5 @@
 #define srv_pin A3  // пин сервы
+#define photo_pin A6 // пин фоторезистора
 #define srv_high_time 5000  // время работы сервы
 
 #include <Wire.h>
@@ -16,12 +17,16 @@ unsigned long last_time = 0;
 bool srv_state = false;
 bool srv_timeout = false;
 
+int photo_val;
+
 void setup() {
   pinMode(srv_pin, OUTPUT);
+  pinMode(photo_pin, INPUT);
   Serial.begin(9600);
   Serial.println("begin");
   bmp.begin();
 
+  photo_val = analogRead(photo_pin);
   start_altitude = getAltitude();
 
 }
@@ -31,12 +36,13 @@ void loop() {
   float last_altitude = current_altitude;
   current_altitude = getAltitude() - start_altitude;
   
-  // определяем точку взлета, точку запуска системы спасения, точку приземления и высчитываем максимальную высоту
+  // определяем необходимые точки, высчитываем максимальную высоту
   if (current_altitude - start_altitude >= 5.0) start_flag = true;
   if (current_altitude > max_altitude) max_altitude = current_altitude;
   if ((max_altitude - current_altitude >= 50)  && start_flag) rec_flag = true;
   if (last_altitude - current_altitude <= 2 && start_flag) land_flag = true;
-
+  if (photo_val - analogRead(photo_pin) >= 10) sep_flag = true;
+  
   // запускаем серву на 2 секунды
   if (rec_flag && !srv_state) {
     srv_state = true && !srv_timeout;
@@ -50,7 +56,7 @@ void loop() {
 
   // отправляем телеметрию
   Serial.println("CanSAD;" + String(millis()) + ";" + String(current_altitude) + ";" + String(start_flag) + ";" + String(sep_flag) + 
-  ";" + String(rec_flag) + ";" + String(land_flag) + "\n");
+                 ";" + String(rec_flag) + ";" + String(land_flag) + "\n");
 }
 
 
