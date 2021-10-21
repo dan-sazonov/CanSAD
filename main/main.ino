e00f5e37e40e46ecbdadaa9e9056d718893b6a7a
Dan Sazonov <p.294803@gmail.com>
2021-10-21 10:43:32 +0300

    Поправил код, добавил комменты

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,48 @@
+# define srv_pin A3
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
-float startAltitude;
-bool startFlag, sepFlag, recFlag, landFlag = false;
-float currentAltitude = 0.0;
-float maxAltitude = 0.0;
-int counter;
+
+float start_altitude;
+bool start_flag, sep_flag, rec_flag, land_flag = false;
+float current_altitude = 0.0;
+float max_altitude = 0.0;
+
+unsigned long last_time;
+bool srv_is_working = false;
 
 void setup() {
-  pinMode(A3, OUTPUT);
+  pinMode(srv_pin, OUTPUT);
   Serial.begin(9600);
   Serial.println("begin");
   bmp.begin();
 
-  startAltitude = getAltitude();
+  start_altitude = getAltitude();
 
 }
 
 void loop() {
   // сохраняем значение прошлой высоты
-  float lastAltitude = currentAltitude;
-  currentAltitude = getAltitude() - startAltitude;
+  float last_altitude = current_altitude;
+  current_altitude = getAltitude() - start_altitude;
+  
   // определяем точку взлета, точку запуска системы спасения, высчитываем максимальную высоту
-  if (currentAltitude - startAltitude >= 5.0) startFlag = true;
-  if (currentAltitude > maxAltitude) maxAltitude = currentAltitude;
-  if ((maxAltitude - currentAltitude >= 50)  && startFlag) recFlag = true;
-  
-  if (recFlag) {
-    digitalWrite(A3, HIGH);
+  if (current_altitude - start_altitude >= 5.0) start_flag = true;
+  if (current_altitude > max_altitude) max_altitude = current_altitude;
+  if ((max_altitude - current_altitude >= 50)  && start_flag) rec_flag = true;
+
+  // запускаем серву на 2 секунды
+  if (rec_flag) {
+    digitalWrite(srv_pin, HIGH);
   }
+
+  // отправляем телеметрию
+  Serial.println("CanSAD;" + String(millis()) + ";" + String(current_altitude) + ";" + String(start_flag) + ";" + String(sep_flag) + 
+  ";" + String(rec_flag) + ";" + String(land_flag) + "\n");
   
-  Serial.println("CanSAD;" + String(millis()) + ";" + String(currentAltitude) + ";" + String(startFlag) + ";" + String(sepFlag) + ";" + String(recFlag) + ";" + String(landFlag) + "\n");
-  
-  // TeamID;TIme;Altitude;A;Start point;Separate point;Recovery point;Landing point \n
 }
 
 
@@ -42,12 +50,8 @@ float getAltitude() {
   sensors_event_t event;
   bmp.getEvent(&event);
     if (event.pressure) {
-    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-    return bmp.pressureToAltitude(seaLevelPressure,event.pressure);
+    float sea_level_pressure = SENSORS_PRESSURE_SEALEVELHPA;
+    return bmp.pressureToAltitude(sea_level_pressure,event.pressure);
   }
-  else
-  {
-    Serial.println("Sensor error");
-    return 0.0;
-  }
+  else return 0.0;
 }
